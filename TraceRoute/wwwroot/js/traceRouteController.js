b4e5526de469a6235f707277ef10a2e58a4654c6
Ben Corn <bcorn@bu.edu>
2017-12-14 22:48:13 -0500

    Connecting controller correctly

--- TraceRoute/wwwroot/js/traceRouteController.js
+++ TraceRoute/wwwroot/js/traceRouteController.js
@@ -14,7 +14,13 @@
           
         }); 
 
-
+        vm.TraceRoute = function()
+        {
+            // add crap in here to happen when button is pressed
+            // make api call to our server, GET request using Angular HTTP req
+            // /api/trace/google.com
+            console.log(vm.Hostname);
+        };
     };
 
 })();
